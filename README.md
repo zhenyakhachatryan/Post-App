de5a05957c2b8a7f5dfaa01c7ad1c561c88f3717
Zhenya <khachatryanzhenya3@gmail.com>
2024-05-23 03:26:40 +0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Post-App" 
+"# Post-App" 
--- app.py
+++ app.py
@@ -339,7 +339,7 @@ if __name__=='__main__':
     with app.app_context():
         db.create_all()
         db.session.close()
-    app.run(debug=True)
+    app.run()
 
 
 
